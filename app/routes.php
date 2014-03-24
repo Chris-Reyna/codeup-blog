d2724ae498db9a1dc258ddcb83c28ee3c8ce272f
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-24 14:05:12 -0500

    html update guess and roll

--- app/routes.php
+++ app/routes.php
@@ -26,14 +26,54 @@ Route::get('/portfolio', function()
         return "This is my portfolio";
 });
 
+// Route::get('/sayhello/{name}', function($name)
+// {
+//     if ($name == "Chris")
+//     {
+//         return Redirect::to('/');
+//     }
+//     else
+//     {
+//         return "Hello, $name!";
+//     }
+// });
+
 Route::get('/sayhello/{name}', function($name)
 {
-    if ($name == "Chris")
-    {
-        return Redirect::to('/');
-    }
-    else
-    {
-        return "Hello, $name!";
-    }
+
+	$data = array(
+		'name' => $name
+		);
+
+    return View::make('my-first-view')->with($data);
 });
+
+Route::get('/rolldice/{roll}/{guess}', function($roll,$guess)
+{
+	$roll = rand(1,6);
+	$data = array(
+		'roll' => $roll,
+		'guess' => $guess
+		// 'message' => $message
+		);
+	// if ($roll == $guess){
+	// 	$message = "Your guess was Correct!";
+	// }else{
+	// 	$message = "Your guess was WRONG!!";
+	// }
+	return View::make('roll-dice')->with($data);
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/views/roll-dice.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>SLOW YOUR ROLL</title>
+</head>
+<body>
+	<h2>Your guess = <?= $guess; ?>!</h2>
+    <h1>Your roll = <?= $roll; ?>!</h1>
+    
+</body>
+</html>
