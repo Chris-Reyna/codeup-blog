0d6468db1a37e67390de1415a46b5b8ed9a0130b
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-25 15:59:23 -0500

    update drop down

--- app/views/Layouts/master.blade.php
+++ app/views/Layouts/master.blade.php
@@ -46,15 +46,11 @@
               <li><a href="{{{ action('HomeController@showResume') }}}">Resumé</a></li>
               <li><a href="{{{ action('HomeController@showBlog') }}}">Blog</a></li>
               <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Assignments<b class="caret "></b></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Social Media<b class="caret "></b></a>
                   <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li class="dropdown-header">Nav header</li>
-                    <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
+                    <li><a href="https://twitter.com/creyna">Twitter</a></li>
+                    <li><a href="https://www.linkedin.com/in/christopherreyna">Linkedin</a></li>
+                    <li><a href="https://github.com/Chris-Reyna">GitHub</a></li>
                   </ul>
                 </li>
             </ul>
