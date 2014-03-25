5df2cccb539df14e03fb7d8818b065855d07a347
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-25 15:06:25 -0500

    initial
    create blog blade

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -1,2 +1,19 @@
 @extends('layouts.master')
 
+@section('content')
+
+<form id="creator" class="form" action="{{{ action('PostsController@store') }}}" method="post">
+	<div>
+		<label for="Title">Title</label>
+		<input type="text" id="Title" name="Title" value="{{{ Input::old('Title')}}}">
+	</div>
+	<div>
+		<label for="Body">Body</label>
+		<textarea id="Body" name="Body" rows="10" cols="100" >{{{ Input::old('Body')}}}</textarea>
+	</div>
+	<button type="submit">Create New Post</button>
+</form>
+
+
+
+@stop
