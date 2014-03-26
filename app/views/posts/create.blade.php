2c360c66432c6776a26e188767d69959f3de854b
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-26 17:19:41 -0500

    added news fields

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -4,12 +4,12 @@
 
 <form id="creator" class="form" action="{{{ action('PostsController@store') }}}" method="post">
 	<div>
-		<label for="Title">Title</label>
-		<input type="text" id="Title" name="Title" value="{{{ Input::old('Title')}}}">
+		<label for="title">Title</label>
+		<input type="text" id="title" name="title" value="{{{ Input::old('Title')}}}">
 	</div>
 	<div>
-		<label for="Body">Body</label>
-		<textarea id="Body" name="Body" rows="10" cols="100" >{{{ Input::old('Body')}}}</textarea>
+		<label for="body">Body</label>
+		<textarea id="body" name="body" rows="10" cols="100" >{{{ Input::old('Body')}}}</textarea>
 	</div>
 	<button type="submit">Create New Post</button>
 </form>
