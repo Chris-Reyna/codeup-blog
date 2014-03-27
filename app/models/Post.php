ef7f56b2017eb77dd0682c3aa9cd0035615da786
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-27 10:59:45 -0500

    added rules to form
    title max of 100
    body max 10,000
    made variable rules public static

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,4 +4,10 @@ class Post extends Eloquent {
 
     protected $table = 'posts';
 
+    //Validation Rules
+    public static $rules = array(
+    	'title'      => 'required|max:100',
+    	'body'       => 'required|max:10000'
+	);
+
 }
