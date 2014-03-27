1a5c67ee2a3c616e155bb4286939b37054062bb3
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-27 11:01:10 -0500

    added code to show individual post by id

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -2,7 +2,19 @@
 
 @section('content')
 
+<div class="blog-post">
+	<h2 class="blog-post-title">{{{ $post->title }}}</h2>
+	<p class="blog-post-meta">{{{ $post->created_at }}} <a href="#">Link</a></p>
+	<p> 
+		<textarea class="form-control" rows="3">{{{ $post->body }}}</textarea>
+	</p>
+</div>	
 
+<hr>
+
+<div>
+	<a href= "{{{ action('PostsController@index')}}}">Return to Posts Listing</a>
+</div>
 
 
 
