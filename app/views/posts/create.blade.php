040e141f14fd28e0d9e0b7356ff9bfb522629fea
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-27 11:14:02 -0500

    added validation to form html
    php code is there a title/body, if has title does it follow the rules,
    if not display error message, else submit input

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -5,11 +5,13 @@
 <form id="creator" class="form" action="{{{ action('PostsController@store') }}}" method="post">
 	<div>
 		<label for="title">Title</label>
-		<input type="text" id="title" name="title" value="{{{ Input::old('Title')}}}">
+		<input type="text" id="title" name="title" value="{{{ Input::old('title')}}}">
+		{{$errors->has('title') ? $errors->first('title', '<span class="help-block">:message</span>'):''}}
 	</div>
 	<div>
 		<label for="body">Body</label>
-		<textarea id="body" name="body" rows="10" cols="100" >{{{ Input::old('Body')}}}</textarea>
+		<textarea id="body" name="body" rows="10" cols="100" >{{{ Input::old('body')}}}</textarea>
+		{{$errors->has('body') ? $errors->first('body', '<span class="help-block">:message</span>'):''}}
 	</div>
 	<button type="submit">Create New Post</button>
 </form>
