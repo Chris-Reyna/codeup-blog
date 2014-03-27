e1065c94937604a93396d49d58a6b3e18abb4306
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-27 11:00:48 -0500

    added links and changed titie to links
    title links link to show single post

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -6,16 +6,21 @@
 				
 @foreach($posts as $post)		
 <div class="blog-post">
-	<h2 class="blog-post-title">{{{ $post->title }}}</h2>
-	<p class="blog-post-meta">{{{ $post->created_at}}} <a href="#">Mark</a></p>
-	<p> {{{ $post->body }}}</p>
+	<a class="blog-post-title" href= "{{{ action('PostsController@show', $post->id)}}}"><h2>{{{ $post->title }}}</h2></a>
+	<p class="blog-post-meta">{{{ $post->created_at }}} <a href="#">Link</a></p>
+	<p> 
+		<textarea class="form-control" rows="3">{{{ $post->body }}}</textarea>
+	</p>
 </div>			
 @endforeach					
 			
-
-
-
-
+<hr>
+<div>
+	<a href= "{{{ action('PostsController@create')}}}">Create New Post</a>
+</div>
+<div>
+	<a href= "{{{ action('PostsController@show')}}}">View A Post</a>
+</div>
 
 
 @stop
