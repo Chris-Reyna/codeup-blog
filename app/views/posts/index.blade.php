44ab764261170a605a266119e3f08325aa9e49ff
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-28 13:01:42 -0500

    formatted date
    timezone day month and h and m

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -7,9 +7,9 @@
 @foreach($posts as $post)		
 <div class="blog-post">
 	<a class="blog-post-title" href= "{{{ action('PostsController@show', $post->id)}}}"><h2>{{{ $post->title }}}</h2></a>
-	<p class="blog-post-meta">{{{ $post->created_at }}} <a href="#">Link</a></p>
+	<p class="blog-post-meta">{{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:m A ') }}} </p>
 	<p> 
-		{{{ $post->body }}}
+		{{{ Str::words($post->body, 10) }}}
 	</p>
 </div>			
 @endforeach					
