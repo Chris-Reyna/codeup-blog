1faddf37808a4c8e8ef1513236f620b13db8fea2
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-28 11:02:47 -0500

    added delete
    btn, jquery

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -6,18 +6,31 @@
 	<h2 class="blog-post-title">{{{ $post->title }}}</h2>
 	<p class="blog-post-meta">{{{ $post->created_at }}} <a href="#">Link</a></p>
 	<p> 
-		<textarea class="form-control" rows="3">{{{ $post->body }}}</textarea>
+		{{{ $post->body }}}
 	</p>
 </div>	
 
 <hr>
 <div>
-	<a href= "{{{ action('PostsController@edit', $post->id)}}}">Edit Post</a>
-</div>
-<div>
+	<a href= "" id="btnDeletePost" >Delete Post</a> |
+
+	<a href= "{{{ action('PostsController@edit', $post->id)}}}">Edit Post</a> |
+
 	<a href= "{{{ action('PostsController@index')}}}">Return to Posts Listing</a>
 </div>
 
+{{Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'delete', 'id' => 'formDeletePost'))}}
+{{Form::close()}}
+@stop
 
+@section('bottom-script')
 
+<script>
+	$('#btnDeletePost').on('click', function (e){
+		e.preventDefault();
+		if(confirm('Are you sure you want to delete this post?')){
+			$('#formDeletePost').submit();
+		}
+	});
+</script>
 @stop
