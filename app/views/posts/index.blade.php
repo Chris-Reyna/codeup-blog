87e4b72d8369fd6656dbb5fa9dfeeb69fd96bd54
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-31 15:11:02 -0500

    removed timzone from blade
    now being called from base model

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -7,8 +7,8 @@
 @foreach($posts as $post)		
 <div class="blog-post">
 	<a class="blog-post-title" href= "{{{ action('PostsController@show', $post->id)}}}"><h2>{{{ $post->title }}}</h2></a>
-	<p class="blog-post-meta">Original Post Date: {{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i A ') }}} </p>
-	<p class="blog-post-meta">Last Update: {{{ $post->updated_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i A ') }}} </p>
+	<p class="blog-post-meta">Original Post Date: {{{ $post->created_at->format('l, F jS Y @ h:i A ') }}} </p>
+	<p class="blog-post-meta">Last Update: {{{ $post->updated_at->format('l, F jS Y @ h:i A ') }}} </p>
 	<p> 
 		{{{ Str::words($post->body, 10) }}}
 	</p>
--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -4,8 +4,8 @@
 
 <div class="blog-post">
 	<h2 class="blog-post-title">{{{ $post->title }}}</h2>
-	<p class="blog-post-meta">Original Post Date: {{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i A ') }}} </p>
-	<p class="blog-post-meta">Last Update: {{{ $post->updated_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i A ') }}} </p>
+	<p class="blog-post-meta">Original Post Date: {{{ $post->created_at->format('l, F jS Y @ h:i A ') }}} </p>
+	<p class="blog-post-meta">Last Update: {{{ $post->updated_at->format('l, F jS Y @ h:i A ') }}} </p>
 	<p> 
 		{{{ $post->body }}}
 	</p>
