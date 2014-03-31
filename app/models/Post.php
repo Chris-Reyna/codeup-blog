9f5993dbce8931a9bc111dac744787f9de8dfd20
Chris-Reyna <christopher.reyna@yahoo.com>
2014-03-31 15:10:19 -0500

    extending base model
    removed time zone function

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,8 +1,6 @@
 <?php
 
-use Carbon\Carbon;
-
-class Post extends Eloquent {
+class Post extends BaseModel {
 
     protected $table = 'posts';
 
@@ -13,11 +11,6 @@ class Post extends Eloquent {
 	);
 
 
-    public function getCreatedAtAttribute($value)
-	{
-	    $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
-	    return $utc->setTimezone('America/Chicago');
-	}
 
 }
 
