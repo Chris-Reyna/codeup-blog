31283a21c9a286de0fa9f8913765cdf8ddb5f296
Chris-Reyna <christopher.reyna@yahoo.com>
2014-04-01 11:43:37 -0500

    relationship post to user
    defines Relationship to a user

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,8 +1,12 @@
 <?php
 
 class Post extends BaseModel {
-
-    protected $table = 'posts';
+	protected $table = 'posts';
+	//defines Relationship to a user
+    public function user()
+	{
+    	return $this->belongsTo('User');
+	}
 
     //Validation Rules
     public static $rules = array(
