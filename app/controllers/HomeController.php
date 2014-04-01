bbb550eaeef0b6083127bf2a7c4cc21c5d3a7f2b
Chris-Reyna <christopher.reyna@yahoo.com>
2014-04-01 15:57:42 -0500

    added long in and logout
    methods

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -32,5 +32,26 @@ class HomeController extends BaseController {
 	{
 		return View::make('posts/index');
 	}
+	public function showLogin()
+	{
+		return View::make('login');
+	}
+	public function doLogin()
+	{
+		if (Auth::attempt(array('email' => Input::get('email'), 'password' => Input::get('password'))))
+		{
+    		return Redirect::intended('/posts');
+		}
+		else
+		{
+    		// login failed, go back to the login screen
+			return Redirect::back()->withInput();
+		}
+	}
+	public function logout()
+	{
+		Auth::logout();
+		return Redirect::action('PostsController@index');
+	}
 
 }
