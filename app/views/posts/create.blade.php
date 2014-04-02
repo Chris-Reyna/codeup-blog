55d338a94b65725c3cdabe889a62231b25d67e69
Chris-Reyna <christopher.reyna@yahoo.com>
2014-04-02 15:12:52 -0500

    removed commented out html added img in blade

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -2,28 +2,25 @@
 
 @section('content')
 						<!--add a comma and the class or id after postcontroller for styling-->
-{{ Form::open(array('action' => 'PostsController@store')) }}
-<!--<form id="creator" class="form" action="{{{ action('PostsController@store') }}}" method="post">-->
+{{ Form::open(array('action' => 'PostsController@store', 'files' => true)) }}
 	<div>
-		<!--<label for="title">Title</label>-->
-		<!--<input type="text" id="title" name="title" value="{{{ Input::old('title')}}}">-->
-		<!--for styling add (title,null or '',array('class'=> 'whatever', 'placeholder'=> 'title', etc))-->
 		{{ Form::label('title', 'Title') }}
 		{{ Form::text('title') }}
 		{{$errors->first('title', '<span class="help-block">:message</span>')}}
 	</div>
 	<div>
-		<!--<label for="body">Body</label>-->
-		<!--<textarea id="body" name="body" rows="10" cols="100" >{{{ Input::old('body')}}}</textarea>-->
 		{{ Form::label('body','Body')}}
 		{{ Form::textarea('body')}}
 		{{$errors->first('body', '<span class="help-block">:message</span>')}}
 	</div>
-	<!--<button type="submit">Create New Post</button>-->
+	<div>
+		{{ Form::file('image')}}
+	</div>
+	<br>
 	<div>
 		{{ Form::submit('Create New Post')}}
 	</div>
-<!--</form>-->
+
 {{ Form::close() }}
 
 
