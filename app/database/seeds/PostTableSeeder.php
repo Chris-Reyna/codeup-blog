c17798be1905bbd6fe3b8fd2880e75aae7719204
Chris-Reyna <christopher.reyna@yahoo.com>
2014-04-02 15:33:59 -0500

    deleted post 2 and added manda with img

--- app/database/seeds/PostTableSeeder.php
+++ app/database/seeds/PostTableSeeder.php
@@ -11,16 +11,15 @@ class PostTableSeeder extends DatabaseSeeder {
         DB::table('posts')->delete();
 
         $post1 = new Post();
-        $post1->title = 'Well done bacon, well done.';
-        $post1->body = 'Bacon ipsum dolor sit amet rump voluptate bacon do enim drumstick. In cow ground round landjaeger magna sint beef non filet mignon. Beef ribs beef pork belly leberkas, pork cupidatat ad nulla in tempor t-bone hamburger proident qui rump. Eu velit kevin cillum ullamco shankle dolore enim in pork chop exercitation minim. Cillum chuck aliquip, filet mignon aliqua leberkas veniam pancetta strip steak irure prosciutto ribeye in ut anim. Turducken voluptate swine enim, magna irure brisket ea reprehenderit exercitation.';
+        $post1->title = 'Manda';
+        $post1->body = 'This is Manda!!'
         $post1->user_id = 1;
+        $post1->img_path = 'blog_img/533c556e94359.jpg'
         $post1->save();
 
         $post2 = new Post();
-        $post2->title = 'Lorem Ipsum';
-        $post2->body = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit commodi aperiam reiciendis. Aspernatur, corporis, necessitatibus atque fuga eum magni magnam vel vero nobis minima aperiam expedita ipsum ea esse eos.
-
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, nostrum dicta voluptatum deleniti assumenda ipsa a. Quo, quos, ratione explicabo accusantium deleniti rem odit adipisci sit ullam nisi laborum dicta.';
+        $post2->title = 'Well done bacon, well done.';
+        $post2->body = 'Bacon ipsum dolor sit amet rump voluptate bacon do enim drumstick. In cow ground round landjaeger magna sint beef non filet mignon. Beef ribs beef pork belly leberkas, pork cupidatat ad nulla in tempor t-bone hamburger proident qui rump. Eu velit kevin cillum ullamco shankle dolore enim in pork chop exercitation minim. Cillum chuck aliquip, filet mignon aliqua leberkas veniam pancetta strip steak irure prosciutto ribeye in ut anim. Turducken voluptate swine enim, magna irure brisket ea reprehenderit exercitation.';
         $post2->user_id = 1;
         $post2->save();
 
