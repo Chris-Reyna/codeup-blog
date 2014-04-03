2e91fa535cf44dcd6cf43e3530661520211b1103
Chris-Reyna <christopher.reyna@yahoo.com>
2014-04-03 14:14:24 -0500

    removing roles table

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -12,5 +12,13 @@ class UserTableSeeder extends DatabaseSeeder{
         $user->lastname = 'Reyna';
         $user->role_id = 1;
         $user->save();
+
+        $user2 = new User();
+	$user2->email = 'auth@codeup.com';
+        $user2->password = 'authPass123!';
+        $user2->firstname = 'Brandon';
+        $user2->lastname = 'Beidel';
+        $user2->role_id = 2;
+        $user2->save();
 	}
 }
