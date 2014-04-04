1bf6cd1c16b9e5c43e6f0012cc21b8b55472e6d1
Chris-Reyna <christopher.reyna@yahoo.com>
2014-04-04 09:57:53 -0500

    Added if Auth check to check if user or guest

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -18,10 +18,12 @@
 
 <hr>
 <div>
+	@if (Auth::check())
 	@if (Auth::user()->canManagePost($post))
 	<a href= "" id="btnDeletePost" >Delete Post</a> |
 
 	<a href= "{{{ action('PostsController@edit', $post->id)}}}">Edit Post</a> |
+	@endif
 	@endif
 	<a href= "{{{ action('PostsController@index')}}}">Return to Posts Listing</a>
 	
