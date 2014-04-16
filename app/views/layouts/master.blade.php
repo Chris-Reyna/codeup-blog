faca6b29a7dca27f6261a40be16e14b81941c0e1
Chris-Reyna <christopher.reyna@yahoo.com>
2014-04-16 15:02:47 -0500

    added font awesome bootstrap and css page links

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,7 +7,9 @@
     <title>Laravel Blog</title>
 	   <!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
   <link href="/css/signin.css" rel="stylesheet">
+  <link href="/css/font-awesome.css" rel="stylesheet">
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 	 <script src="/js/jquery.js"></script>
