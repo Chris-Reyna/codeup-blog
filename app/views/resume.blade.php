8eddedde12a4d222c6d046ee7d393fdfda9857cf
Chris-Reyna <christopher.reyna@yahoo.com>
2014-04-16 15:03:28 -0500

    added content and links to social media

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -6,7 +6,7 @@
 
           <div class="blog-header">
             <h1 class="blog-title">Christopher Reyna</h1>
-            <p class="lead blog-description">Junior Software Developer</p>
+            <p class="lead blog-description">FUll Stack Web Developer</p>
           </div>
 
           <div class="row">
@@ -20,22 +20,33 @@
                   <h3>Email</strong></h3>
                   <p><a href="christopher.reyna@yahoo.com">christopher.reyna@yahoo.com</a></p>
                 <h3>Social Media</h3> 
-                  <p><a href="https://twitter.com/creyna">Twitter</a></p> 
-                  <p><a href="https://www.linkedin.com/in/christopherreyna">Linkedin</a></p>
-                  <p><a href="https://github.com/Chris-Reyna">GitHub</a></p>
+                  <p><a class="btn btn-social-icon btn-lg btn-twitter" href="https://twitter.com/creyna" target="_blank"><i class="fa fa-twitter"></i></a><br /></p> 
+                  <p>
+                    <a class="btn btn-social-icon btn-lg btn-linkedin" href="https://www.linkedin.com/in/christopherreyna" target="_blank"><i class="fa fa-linkedin"></i></a>
+                  </p>
+                  <p>
+                    <a class="btn btn-social-icon btn-lg btn-github" href="https://github.com/Chris-Reyna" target="_blank" ><i class="fa fa-github-alt"></i></a><br>
+                  </p>
               </div><!-- /.blog-post -->
               <hr>
               <div class="blog-post">
                 <h2 class="blog-post-title">WORK EXPERIENCE</h2>
                 <h3>Texas Department of Criminal Justice</h3>
-                  <p>March 2010 - February 2014</p> 
+                  <p>March 2010 - February 2014</p>
+                  <p>March/ 2010-PRESENT – STATE PAROLE OFFICER, TEXAS DEPARTMENT OF CRIMINAL JUSTICE
+                  District Parole Officer II: Monitor Offender compliance with all laws and conditions of supervision. Conduct investigations, compile evidence, prepare social and developmental histories, document offender activities while on supervision and maintain required records; prepare evaluations, recommendations, and related reports to include data entry, retrieval, and data searches. Conduct contacts at offender residences, jails, correctional facilities, courthouses, and with other agencies such as local and Federal law enforcement agencies in order to obtain offender information. Evaluate offender adjustments and recommend revisions to parole supervision plan. Perform criminal information searches and retrieval using NCIC/TCIC criminal history system. Conduct, field, and home interviews of offenders under multiple levels of supervision. 
+                  Super Intensive Supervision Program (SISP) Officer: Interview and counsel a caseload of high-risk offenders placed on the Super-Intensive Supervision GPS monitoring program; Ensure GPS monitoring equipment is in working order to track offender movement. This case load also involves working with sex offenders, high ranking gang members, high risk violent offenders and mentally ill offender populations. Daily activities include installation and retrieval of equipment, meeting with offenders on a weekly basis for scheduling approved activity, identifying trends and tendencies of travel and activities, investigate deviations from normal approved activities, answering alerts in a timely fashion, offender tracking and troubleshooting equipment errors.</p> 
                 <h3>Laurel Ridge Treatment Center</h3>
                   <p>August 2000 - Present</p>
+                  <p>2008-Specialized Therapeutic Services Activity Specialist: Facilitate recreational therapy groups; provide self awareness, positive coping skills, positive social skills; Assist development of motor skills and leisure awareness.</p>
+                  <p>PRN-Staffing Coordinator: Supervised the placement of nurses and mental health workers to units according to census and acuity of the unit for daily shifts.</p> 
+                  <p>2002-Shift Supervisor: Supervised staff of five mental health workers, delegated work assignments, and was responsible for providing a safe program for a 23-bed adolescent unit and an 18-bed adolescent Sex-offender unit.</p>
+                  <p>2000-Mental Health Worker: Provided a structured and safe environment for adjudicated adolescents, children and adults. Conducted skills training sessions, followed treatment plans, documented on day to day behaviors and interventions.</p> 
               <hr>
               <div class="blog-post">
                 <h2 class="blog-post-title">Education</h2>
                 <h3><strong>LAMP+J Software Development Bootcamp</strong></h3>
-                  <p>Graduated April 2014</p>
+                  <p>Completed April 2014</p>
                   <a href="http://www.codeup.com/">Codeup</a>
                 <h3>Bachelors of Arts-Criminal Justice</h3>
                   <p>University of Texas San Antonio</p>
